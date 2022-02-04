a4688877a1abd685a7fe9b4df4530e01076b7030
dancal <dancal@empas.com>
2022-02-04 11:11:35 +0900

    add init files

---
+++ updown_script/shutdown_lcd_finally.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from spotmicroai.lcd_screen_controller import LCD_16x2_I2C_driver
+import socket
+
+#i2c_address = Config().get('lcd_screen_controller[0].lcd_screen[0].address')
+
+i2c_address = "0x27"
+
+lcd_screen = LCD_16x2_I2C_driver.lcd(address=int(i2c_address, 0))
+
+def shutdown():
+
+    lcd_screen.lcd_clear()
+    lcd_screen.lcd_display_string("Done.", 1)
+    lcd_screen.lcd_display_string("Ready Power Off", 2)
+
+shutdown()
